b84a44fcfe6254f2667fb0653949be4456d1c4b4
matviy101 <syskomatviy2@gmail.com>
2026-08-25 14:46:29 +0100

    hw7react

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Section = styled.div`
+font-family: sans-serif;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+`
+const List = styled.li`
+display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 30px;
+  margin-bottom: 10px;
+  font-size: 20px;
+  `
+
+  const Button = styled.button`
+  width: 120px;
+  height: 40px;
+  border-radius: 10px;
+  border: none;
+  background-color: red;
+  color: white;
+  font-size: 20px;
+  cursor: pointer;
+   `
+class TaskList extends Component {
+  static tasks = [
+    { id: 1, text: "task1" },
+    { id: 2, text: "task2" },
+    { id: 3, text: "task3" },
+  ];
+
+  deleteTask(id) {
+    TaskList.tasks = TaskList.tasks.filter(
+      (task) => task.id !== id
+    );
+
+    this.forceUpdate();
+  }
+
+  render() {
+    return (
+      <Section>
+        <h1>Task List</h1>
+
+        <ul>
+          {TaskList.tasks.map((task) => (
+            <List key={task.id}>
+              {task.text}
+              <Button onClick={() => this.deleteTask(task.id)}>Delete</Button>
+            </List>
+          ))}
+        </ul>
+      </Section>
+    );
+  }
+}
+
+export default TaskList;
